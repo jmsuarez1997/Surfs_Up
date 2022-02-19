e5343bf32e31f6ede227b73785d095ab092a876a
Juana Suarez <jmsuarez1997@gmail.com>
2022-02-19 10:30:00 -0600

    adding app.py file

--- app.py
+++ app.py
@@ -1,7 +1,38 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask
 
+# from flask import Flask
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
+#################################################
+# Flask Routes
+#################################################
 @app.route('/')
 def hello_world():
     return 'Hello world'    
